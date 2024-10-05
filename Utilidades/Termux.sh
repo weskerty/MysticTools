2c39ad14c0fd3b9688a7a281a335fb67286c3712
Weskerty <marcoygor0@gmail.com>
2024-10-05 15:40:30 -0400

    Cambio Repo

--- Utilidades/Termux.sh
+++ Utilidades/Termux.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
-wget https://raw.githubusercontent.com/BrunoSobrino/TheMystic-Bot-MD/refs/heads/master/web/Guias/Utilidades/.bashrc -O ~/.bashrc
+wget https://raw.githubusercontent.com/weskerty/MysticTools/refs/heads/main/Utilidades/.bashrc -O ~/.bashrc
 proot-distro login debian -- bash -c "
   apt-get update && \ 
   curl -fsSL https://deb.nodesource.com/setup_current.x -o nodesource_setup.sh && \
   bash nodesource_setup.sh
   apt-get install gcc make wget curl git ffmpeg imagemagick python3 python3-pip nodejs -y && \
-  wget https://raw.githubusercontent.com/BrunoSobrino/TheMystic-Bot-MD/refs/heads/master/web/Guias/Utilidades/update.sh -O ~/update.sh && \
-  mkdir -p ~/script && wget https://raw.githubusercontent.com/BrunoSobrino/TheMystic-Bot-MD/refs/heads/master/web/Guias/Utilidades/mystic.sh -O ~/script/mystic.sh && \
-  wget  https://raw.githubusercontent.com/BrunoSobrino/TheMystic-Bot-MD/refs/heads/master/web/Guias/Utilidades/alive.sh -O ~/alive.sh && \
+  wget https://raw.githubusercontent.com/weskerty/MysticTools/refs/heads/main/Utilidades/update.sh -O ~/update.sh && \
+  mkdir -p ~/script && wget https://raw.githubusercontent.com/weskerty/MysticTools/refs/heads/main/Utilidades/mystic.sh -O ~/script/mystic.sh && \
+  wget  https://raw.githubusercontent.com/weskerty/MysticTools/refs/heads/main/Utilidades/alive.sh -O ~/alive.sh && \
   chmod +x ~/update.sh && \
   chmod +x ~/script/mystic.sh && \
   chmod +x ~/alive.sh && \
