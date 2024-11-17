3741c1ad140fe9fe4ff31eb85e1670a669d5b22d
Weskerty <marcoygor0@gmail.com>
2024-11-17 02:39:52 -0300

    lev

--- Utilidades/Lev/Termux.sh
+++ Utilidades/Lev/Termux.sh
@@ -17,8 +17,8 @@ proot-distro login debian -- bash -c "
 
 #RUN in TERMUX: apt update -y && yes | apt upgrade && pkg install -y wget && wget -O - https://raw.githubusercontent.com/weskerty/MysticTools/refs/heads/main/Utilidades/Lev/Termux.sh | bash 
 
-💡 Prevent Force Close (Optional)
-In case Termux keeps closing on you, try this. It's better to do it Command by Command, but Copying and Pasting Everything Should Work Too.
+# 💡 Prevent Force Close (Optional)
+# In case Termux keeps closing on you, try this. It's better to do it Command by Command, but Copying and Pasting Everything Should Work Too.
 
-On some phones, the "Disable Child Process Restrictions" option appears in Developer Options
-You can do this process from your PC or from Termux itself by following this tutorial ↗️ 
+# On some phones, the "Disable Child Process Restrictions" option appears in Developer Options
+# You can do this process from your PC or from Termux itself by following this tutorial https://gist.github.com/kairusds/1d4e32d3cf0d6ca44dc126c1a383a48d
