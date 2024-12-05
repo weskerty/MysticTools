8badc5b95ebd18ff2130addc2eab42046dd244ec
laWiskaPY <82781997+weskerty@users.noreply.github.com>
2024-12-05 13:54:07 -0300

    Create changeID.sh

---
+++ Utilidades/Lev/changeID.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+proot-distro login debian -- bash -c "
+  # Preguntar por SESSION_ID
+  echo -e '\e[36mDo you have a SESSION_ID scanned today? (y/n):\e[0m' && \
+  read -r HAS_SESSION_ID && \
+  if [[ \"\$HAS_SESSION_ID\" == \"y\" ]]; then \
+    echo -e '\e[36mEnter Your SESSION_ID:\e[0m' && \
+    read -r SESSION_ID && \
+    echo \"SESSION_ID=\$SESSION_ID\" >> ~/levanter/config.env; \
+  else \
+    sed -i '/^SESSION_ID=/d' ~/levanter/config.env; \
+  fi && \
+  npm start 
+"
