e92a6d39ee81e563fb4fedfae6262529f07331e1
laWiskaPY <82781997+weskerty@users.noreply.github.com>
2025-01-16 14:42:06 -0300

    Update Termux2.sh

--- Utilidades/Lev/Termux2.sh
+++ Utilidades/Lev/Termux2.sh
@@ -1,21 +1,22 @@
 #!/bin/bash
-pkg install -y proot-distro && proot-distro install debian
-curl -fsSL https://raw.githubusercontent.com/weskerty/MysticTools/refs/heads/main/Utilidades/Lev/.bashrc > ~/.bashrc && \
-proot-distro login debian -- bash -c "
-  apt-get update && \ 
-  curl -fsSL https://deb.nodesource.com/setup_lts.x -o nodesource_setup.sh && \
-  bash nodesource_setup.sh && \
-  apt-get install sudo nano gcc make wget curl git ffmpeg nodejs -y && \
-  wget  https://raw.githubusercontent.com/weskerty/MysticTools/refs/heads/main/Utilidades/Lev/lev.sh -O ~/lev.sh && \
-  chmod +x ~/lev.sh && \
-  git clone https://github.com/lyfe00011/levanter.git levanter && \
-  wget https://raw.githubusercontent.com/weskerty/MysticTools/refs/heads/main/Utilidades/Lev/config.env -O ~/levanter/config.env && \
+pkg install -y tur-repo x11-repo
+pkg install wget -y
+curl -fsSL https://raw.githubusercontent.com/weskerty/MysticTools/refs/heads/main/Utilidades/Lev/.bashrc > ~/.bashrc 
+  apt-get update
+apt-get install nano gcc clang make wget curl git ffmpeg nodejs-lts pkg-config 
+  wget https://github.com/lzhiyong/termux-ndk/releases/download/android-ndk/android-ndk-r27b-aarch64.zip 
+  unzip ~/android-ndk-r27b-aarch64.zip -d ~/android-ndk
+  wget  https://raw.githubusercontent.com/weskerty/MysticTools/refs/heads/main/Utilidades/Lev/lev.sh -O ~/lev.sh 
+    wget  https://raw.githubusercontent.com/weskerty/MysticTools/refs/heads/main/Utilidades/Lev/include.gypi -O ~/.gyp/include.gypi 
+  chmod +x ~/lev.sh 
+  git clone https://github.com/lyfe00011/levanter.git levanter
+  wget https://raw.githubusercontent.com/weskerty/MysticTools/refs/heads/main/Utilidades/Lev/config.env -O ~/levanter/config.env 
   
 
   
-  npm install yarn pm2 -g && \
-  cd levanter && \
-  yarn install && \
+  npm install yarn pm2 -g 
+  cd levanter 
+  yarn install 
 
   # Preguntar por SESSION_ID
   echo -e '\e[36mDo you have a SESSION_ID scanned today? (y/n):\e[0m' && \
@@ -27,4 +28,3 @@ proot-distro login debian -- bash -c "
   fi && \
   
   npm start 
-"
