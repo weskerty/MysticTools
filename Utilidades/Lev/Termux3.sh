1e664517434417a98e8edbf05b4b3d20c17d9687
laWiskaPY <82781997+weskerty@users.noreply.github.com>
2025-01-16 16:37:03 -0300

    Update Termux3.sh

--- Utilidades/Lev/Termux3.sh
+++ Utilidades/Lev/Termux3.sh
@@ -8,7 +8,7 @@ pkg install -y tur-repo x11-repo && \
 # Actualizar Repositorio
 apt-get update && \
 # Instalar Programas ""Necesarios"". No recuerdo cual es la dependencia necesaria, algun dia vere cuales estan de mas.
-pkg install -ypython nano clang make git ffmpeg nodejs-lts pkg-config libxml2 libxslt matplotlib xorgproto rust binutils wget build-essential libvips python-pip glib openjdk-21 file libsqlite sqlite && \
+pkg install -y python nano clang make git ffmpeg nodejs-lts pkg-config libxml2 libxslt matplotlib xorgproto rust binutils wget build-essential libvips python-pip glib openjdk-21 file libsqlite sqlite && \
 pip install cython wheel setuptools python-dotenv && \
 export ANDROID_NDK_HOME=~/android-ndk/android-ndk-r27b && \
 export PATH=$ANDROID_NDK_HOME:$PATH && \
