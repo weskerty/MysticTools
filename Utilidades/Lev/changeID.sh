da69ef21afd36f28f7e1956c782c2c153f823cd9
laWiskaPY <82781997+weskerty@users.noreply.github.com>
2025-05-29 17:44:50 -0300

    Update changeID.sh

--- Utilidades/Lev/changeID.sh
+++ Utilidades/Lev/changeID.sh
@@ -5,12 +5,16 @@ cd levanter && \
   # Preguntar por SESSION_ID
   echo -e '\e[36mDo you have a SESSION_ID scanned today? (y/n):\e[0m' && \
   read -r HAS_SESSION_ID && \
-  if [[ \"\$HAS_SESSION_ID\" == \"y\" ]]; then \
+  if [[ \"\$HAS_SESSION_ID\" == \"y\" || \"\$HAS_SESSION_ID\" == \"Y\" ]]; then \
     echo -e '\e[36mEnter Your SESSION_ID:\e[0m' && \
     read -r SESSION_ID && \
-    echo \"SESSION_ID=\$SESSION_ID\" >> ~/levanter/config.env; \
+    sed -i '/^SESSION_ID=/d' ~/levanter/config.env && \
+    echo \"SESSION_ID=\$SESSION_ID\" >> ~/levanter/config.env && \
+    echo -e '\e[32mSESSION_ID added successfully!\e[0m'; \
   else \
-    sed -i '/^SESSION_ID=/d' ~/levanter/config.env; \
+    sed -i '/^SESSION_ID=/d' ~/levanter/config.env && \
+    echo -e '\e[33mSESSION_ID removed from config.env\e[0m'; \
   fi && \
+  echo -e '\e[36mStarting application...\e[0m' && \
   npm start 
 "
