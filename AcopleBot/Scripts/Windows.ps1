ecb5ce19b020017345e9b6fd68d7a9d20e105116
laWiskaPY <82781997+weskerty@users.noreply.github.com>
2025-08-11 15:03:54 -0300

    Update Windows.ps1

--- AcopleBot/Scripts/Windows.ps1
+++ AcopleBot/Scripts/Windows.ps1
@@ -1,1 +1,34 @@
+$programas = @(
+    "libwebp",
+    "jq",
+    "git",
+    "nodejs",
+    "Microsoft.Edit",
+    "python3"
+)
 
+$repoUrl = "https://github.com/weskerty/AcopleBot.git"
+$destinoRepo = "$env:USERPROFILE\AcopleBot"
+
+foreach ($prog in $programas) {
+    $estado = winget list --id $prog -e
+    if (-not $estado) {
+        Write-Host "Instalando $prog..." -ForegroundColor Cyan
+        winget install $prog -e --scope machine --source winget --accept-source-agreements --accept-package-agreements
+    } else {
+        Write-Host "$prog ya instalado." -ForegroundColor Green
+    }
+}
+
+$env:PATH = [System.Environment]::GetEnvironmentVariable("Path", "Machine") + ";" + `
+            [System.Environment]::GetEnvironmentVariable("Path", "User")
+
+if (Test-Path $destinoRepo) {
+    Write-Host "Ya Clonado $destinoRepo" -ForegroundColor Yellow
+} else {
+    git clone $repoUrl $destinoRepo
+}
+
+cd $destinoRepo
+npm install
+npm start
